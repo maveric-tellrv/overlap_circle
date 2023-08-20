6e69091d09feae8163b7c8313e374069d705cd3b
Rohit Vyas <vyasrohitvyas@gmail.com>
2023-08-20 17:17:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ import pytest
     ([(1,3,0.7),(2,3,0.4),(3,3,0.9)],[(3, 3, 0.9)]),
     ([(0.5, 0.5, 0.5),(0.7, 0.7, 0.4),(1,1,1),(1.5, 1.5, 1.1),(4,4,0.7),(2.9,3,1)],[(1.5,1.5,1.1)])
 ])
-def test_addition(ctuple, expected):
+def test_reduce_cluster(ctuple, expected):
     
     print("INPUT\n",ctuple)
     result = reduce_clusters(ctuple)
