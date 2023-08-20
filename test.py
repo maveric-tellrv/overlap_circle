2f74a5bd750baeab9e21c13132bb12d52fd006cf
Rohit Vyas <vyasrohitvyas@gmail.com>
2023-08-20 17:06:15 +0530

    Update test.py
    updated the pytest file

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 from cluster_utils import reduce_clusters
 
+import pytest
 
 
-ctuple2 = [(0.5, 0.5, 0.5), (0.7, 0.7, 0.4),(1.5, 1.5, 1.1),(4,4,0.7)]
-ctuple3 = [(1.5, 1.5, 1.1),(4,4,0.7)]
-ctuple4 = [(1,3,0.7),(2,3,0.4),(3,3,0.9)]
-ctuple5 = [(0.5, 0.5, 0.5),(0.7, 0.7, 0.4),(1,1,1),(1.5, 1.5, 1.1),(4,4,0.7),(2.9,3,1)]
 
-test_data = [ctuple2,ctuple3,ctuple4,ctuple5]
-for ctup in test_data:
-    print("INPUT DATAPOINTS \n",ctup)
-    print("OUTPUT CLUSTER\n",reduce_clusters(ctup))
+# Define the parameters and expected results
+@pytest.mark.parametrize("ctuple, expected", [
+    ([(0.5, 0.5, 0.5), (0.7, 0.7, 0.4),(1.5, 1.5, 1.1),(4,4,0.7)],[(4, 4, 0.7), (1.5, 1.5, 1.1)]),
+    ([(1.5, 1.5, 1.1),(4,4,0.7)],[(1.5, 1.5, 1.1), (4, 4, 0.7)]),
+    ([(1,3,0.7),(2,3,0.4),(3,3,0.9)],[(3, 3, 0.9)]),
+    ([(0.5, 0.5, 0.5),(0.7, 0.7, 0.4),(1,1,1),(1.5, 1.5, 1.1),(4,4,0.7),(2.9,3,1)],[(1.5,1.5,1.1)])
+])
+
+def test_addition(ctuple, expected):
+    result = reduce_clusters(ctuple)
+    assert result == expected
+
