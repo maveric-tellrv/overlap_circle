d5a2ca676348df3a6cd5c7f23bb64f04dc876ae4
Rohit Vyas <vyasrohitvyas@gmail.com>
2023-08-20 17:13:10 +0530

    Update test.py
    Input /expected in print

--- test.py
+++ test.py
@@ -2,8 +2,6 @@ from cluster_utils import reduce_clusters
 
 import pytest
 
-
-
 # Define the parameters and expected results
 @pytest.mark.parametrize("ctuple, expected", [
     ([(0.5, 0.5, 0.5), (0.7, 0.7, 0.4),(1.5, 1.5, 1.1),(4,4,0.7)],[(4, 4, 0.7), (1.5, 1.5, 1.1)]),
@@ -11,8 +9,9 @@ import pytest
     ([(1,3,0.7),(2,3,0.4),(3,3,0.9)],[(3, 3, 0.9)]),
     ([(0.5, 0.5, 0.5),(0.7, 0.7, 0.4),(1,1,1),(1.5, 1.5, 1.1),(4,4,0.7),(2.9,3,1)],[(1.5,1.5,1.1)])
 ])
-
 def test_addition(ctuple, expected):
+    
+    print("INPUT\n",ctuple)
     result = reduce_clusters(ctuple)
+    print("EXPECTED\n",expected)
     assert result == expected
-
